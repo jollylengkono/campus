bb79e685a148a2af73543d38be80e858b8d876fb
JollySL <jolly.lengkono@gmail.com>
2017-11-18 10:25:08 +0700

    Cleanup some unused import

--- src/pages/my-courses/my-courses.ts
+++ src/pages/my-courses/my-courses.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, ActionSheetController } from 'ionic-angular';
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
-import { AngularFireAuth } from 'angularfire2/auth';
 import { CourseProvider } from '../../providers/course/course';
 
 /**
@@ -22,8 +21,8 @@ export class MyCoursesPage {
   private studentCoursesRef: AngularFirestoreCollection<any>;
   private studentCourses: Observable<any[]>; //Expecting single record
 
-  constructor(public navCtrl: NavController, private afs: AngularFirestore, private afAuth: AngularFireAuth, public actionSheetCtrl: ActionSheetController, private courseProvider: CourseProvider) {
-    this.studentCoursesRef = courseProvider.getStudentCoursesRef();
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController, private courseProvider: CourseProvider) {
+    this.studentCoursesRef = this.courseProvider.getStudentCoursesRef();
     this.studentCourses = this.studentCoursesRef.snapshotChanges().map(changes => {
       return changes.map(c => {
         const data = c.payload.doc.data();
